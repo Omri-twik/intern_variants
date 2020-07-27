9d87f2a939ca50fce5b30c4cade503d5cfe2b5aa
mitchneides <mitch.neides@gmail.com>
2020-07-27 12:23:02 +0300

    updates speech to text file

--- mitch/speech_to_text.js
+++ mitch/speech_to_text.js
@@ -1,76 +1,116 @@
+// Problems:
+// 1. Hiding extra "out of place" buttons that sometimes get rendered from media queries
+//	      a. button_validity_check(x,y) doesn't work properly for every situation yet
+// 2. Displaying buttons on sites like https://colourpop.com/, where the search box is
+//	  rendered upon clicking the search icon
+// 3. On scroll, some sites have fixed search area and some move with the scroll of the page
 
 
-(function init_page() {
+function init_page() {
 	// create and add icon cdn to document head
 	let icon_link = document.createElement("LINK");
 	icon_link.setAttribute("rel", "stylesheet");
 	icon_link.setAttribute("href", "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css");
 	document.head.appendChild(icon_link);
 
-	// append buttons to all search boxes that are inside of forms
-	set_dictate_buttons();
-
 	// set event for window resize
 	let body = document.getElementsByTagName("BODY")[0];
 	body.setAttribute("onresize", "recalc_button_position()");
+}
 
 
-
-
-
-// ***************************************************************************************************************
-	// handle search bar icons
-	let all_buttons = document.getElementsByTagName("BUTTON");
-	for (let b of all_buttons) {
-		for (let c of b.classList) {
-			if (c.includes('search')) {
-				// b.setAttribute("onmouseover", "recalc_button_position()");
-				b.setAttribute("onmouseout", "set_dictate_buttons()");
-			}
-		}
-	}
-// ***************************************************************************************************************
-
-
-
-
-
-})()
-
-
-function set_dictate_buttons() {
+function find_visible_search_boxes() {
 	let textInputs = document.getElementsByTagName("INPUT");
 	for (let ti in textInputs) {
-		if (check_for_form_parent(textInputs[ti]) == true) {
-			let container = textInputs[ti].parentElement;
-			if (textInputs[ti]['placeholder']) {
-				if (textInputs[ti]['type'] == 'text' || textInputs[ti]['type'] == 'email' || textInputs[ti]['type'] == 'search') {
-					let btn = create_button();
-					btn.setAttribute("ID", "button_for_" + ti);
-					btn.addEventListener('click', function() {
-						recognition.start();
-						sessionStorage.setItem("current_text_box", "search_target_" + ti);
-						let mics = document.getElementsByClassName("fa-microphone");
-						for (let mic of mics) {
-							mic.style.color = '#8b0000'
-						}
-						container.addEventListener("click", function(event){
-							event.returnValue = false;
-						});
-					});
-
-					set_button_position(textInputs[ti], btn);
-					document.body.appendChild(btn);
-
-					textInputs[ti].setAttribute("ID", "search_target_" + ti);
-					textInputs[ti].removeAttribute('autocomplete');
-					textInputs[ti].classList.add("ti_tw");
-					console.log("[+] Dictate button created");
+		if (textInputs[ti].offsetWidth > 0 && textInputs[ti].offsetHeight > 0) {
+			if (check_for_form_parent(textInputs[ti]) == true) {
+				let container = textInputs[ti].parentElement;
+				if (textInputs[ti]['placeholder']) {
+					if (textInputs[ti]['type'] == 'text' || textInputs[ti]['type'] == 'email' || textInputs[ti]['type'] == 'search') {
+						textInputs[ti].setAttribute("ID", "search_target_" + ti);
+						textInputs[ti].removeAttribute('autocomplete');
+						textInputs[ti].classList.add("search_box_tw");
+						console.log("[+] Visible textbox captured");
+					}
 				}
 			}
 		}
 	}
 }
+
+
+function append_buttons() {
+	let search_boxes = document.getElementsByClassName("search_box_tw");
+	for (let box of search_boxes) {
+		let container = box.parentElement;
+		let box_id = box.id.slice(box.id.lastIndexOf('_') + 1, box.id.length);
+		let btn = create_button();
+		btn.setAttribute("ID", "button_for_" + box_id);
+		btn.addEventListener('click', function() {
+			recognition.start();
+			sessionStorage.setItem("current_text_box", "search_target_" + box_id);
+			let mics = document.getElementsByClassName("fa-microphone");
+			for (let mic of mics) {
+				mic.style.color = '#8b0000'
+			}
+			container.addEventListener("click", function(event){
+				event.returnValue = false;
+			});
+		});
+
+		set_button_position(box, btn);
+		document.body.appendChild(btn);
+	}
+	// find and remove extra buttons
+	// for (let box of search_boxes) {
+	// 	let box_id_num = box.id.slice(-1);
+	// 	let corr_btn = document.getElementById("button_for_" + box_id_num);
+	// 	button_validity_check(box, corr_btn);
+	// 	let final_button = document.getElementById("button_for_" + box_id_num);
+	// 	if (final_button) {
+	// 		final_button.style.display = 'block';
+	// 	}
+	// }
+}
+
+
+// bugs - sometimes removes target button *************************************************
+function button_validity_check(s_box, button) {
+	console.log("current", button);
+	let search_rect = s_box.getBoundingClientRect();
+	let s_top = search_rect.top;
+	let s_right = search_rect.right;
+	let s_bottom = search_rect.bottom;
+	let s_left = search_rect.left;
+
+	let all_buttons = document.getElementsByClassName("dictate_btn");
+	for (let b of all_buttons) {
+		console.log(b, button);
+		if (b != button) {
+			let b_left_str = b.style.left;
+			let b_left = Number(b_left_str.slice(0, -1));
+			b_left = (b_left / 100) * window.innerWidth;
+			b_right = b_left + b.offsetWidth;
+			let b_top_str = b.style.top;
+			let b_top = Number(b_top_str.slice(0, -2));
+			b_bottom = b_top + b.offsetHeight;
+			console.log(b_top, b_right, b_bottom, b_left);
+			if (b_left > s_left && b_right < s_right) {
+				if (b_top < s_top || b_bottom > s_bottom) {
+					b.style.display = 'none';
+					b.remove();
+					console.log("removed", b);
+				}
+			}
+		}
+	}
+}
+
+
+// executions
+init_page();
+find_visible_search_boxes();
+append_buttons();
 
 
 function check_for_form_parent(el) {
@@ -87,117 +127,6 @@ function check_for_form_parent(el) {
 		}
 	}
 	return false;
-}
-
-
-function set_button_position(target, button) {
-	if (isElementInViewport(button) == true && target) {
-		let search_rect = target.getBoundingClientRect();
-		let btn_rect = button.getBoundingClientRect();
-
-		let right = search_rect.right;
-		let right_pct = (right / window.innerWidth) * 100;
-		let button_left = right_pct;
-
-		let top = search_rect.top;
-		let button_top = (top + 3);
-
-		if (document.getElementsByTagName("HEADER")[0].style.position == 'FIXED') {
-			button.style.position = 'fixed';
-		} else {
-			button.style.position = 'absolute';
-		}
-
-		let size = window.innerWidth;
-		// 1080 ///////////////////////////////////////////////////////////
-		if (size >= 1600) {
-			button.style.left = (button_left - 1.5) + "%";
-		} else if (1400 <= size < 1600) {
-			button.style.left = (button_left - 1.8) + "%";
-		} else if (1080 <= size < 1400) {
-			button.style.left = (button_left - 3.8) + "%";
-		} else if (960 <= size < 1080) {
-			button.style.left = (button_left - 5) + "%";
-			console.log(button_left);
-		} else if (900 <= size < 960) {
-			button.style.left = (button_left - 10) + "%";
-		} else if (600 <= size < 900) {
-			button.style.left = (button_left - 5) + "%";
-		} else if (size < 600) {
-			button.style.left = (button_left - 40) + "%";
-		}
-
-		button.style.top = (button_top + 1.4) + "px";
-	} else {
-		button.style.display = "none";
-	}
-}
-
-
-function recalc_button_position() {
-	console.log("[+] Calc");
-	let buttons = document.getElementsByClassName("dictate_btn");
-	for (let button of buttons) {
-		button.style.display = "none";
-		console.log("true");
-	}
-	var rtime;
-	var timeout = false;
-	var delta = 200;
-	$(window).resize(function() {
-		console.log("RE");
-	    rtime = new Date();
-	    if (timeout === false) {
-	        timeout = true;
-	        setTimeout(resizeend, delta);
-	    }
-	});
-
-	function resizeend() {
-		console.log("end");
-	    if (new Date() - rtime < delta) {
-	        setTimeout(resizeend, delta);
-	    } else {
-	        timeout = false;
-
-	        let btns = document.getElementsByClassName("dictate_btn");
-			for (let btn of btns) {
-				let btn_id = btn.id;
-				let id_num = btn_id.slice(-1);
-				let tar = document.getElementById("search_target_" + id_num);
-				set_button_position(tar, btn);
-				btn.style.display = 'block';
-			}
-			console.log("done");
-	    }               
-	}
-}
-
-
-function display_buttons() {
-	console.log("[+] Disp");
-	let btns = document.getElementsByClassName("dictate_btn");
-	for (let btn of btns) {
-		if (isElementInViewport(btn)) {
-			btn.style.display = 'block';
-		}
-	}
-}
-
-
-function isElementInViewport(el) {
-    if (typeof jQuery === "function" && el instanceof jQuery) {
-        el = el[0];
-    }
-
-    var rect = el.getBoundingClientRect();
-
-    return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && /* or $(window).height() */
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth) /* or $(window).width() */
-    );
 }
 
 
@@ -218,6 +147,56 @@ function create_button() {
 }
 
 
+function set_button_position(s_box, button) {
+	let search_rect = s_box.getBoundingClientRect();
+
+	let right = search_rect.right;
+	let right_pct = (right / window.innerWidth) * 100;
+	let button_left = right_pct;
+
+	let top = search_rect.top;
+	let button_top = (top + 3);
+
+	button.style.left = (button_left - 1.5) + "%";
+	button.style.top = (button_top + 1.4) + "px";
+	button.style.position = 'absolute';
+	// button.style.display = 'none';
+	button.style.display = 'block';
+}
+
+
+function recalc_button_position() {
+	let buttons = document.getElementsByClassName("dictate_btn");
+	for (let button of buttons) {
+		button.remove();
+	}
+	var rtime;
+	var timeout = false;
+	var delta = 200;
+	$(window).resize(function() {
+	    rtime = new Date();
+	    if (timeout === false) {
+	        timeout = true;
+	        setTimeout(resizeend, delta);
+	    }
+	});
+
+	function resizeend() {
+	    if (new Date() - rtime < delta) {
+	        setTimeout(resizeend, delta);
+	    } else {
+	        timeout = false;
+	        find_visible_search_boxes();
+			append_buttons();
+	    }               
+	}
+}
+
+
+
+
+
+// speech-to-text
 var message = "";
 var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
 var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList;
@@ -271,9 +250,3 @@ recognition.onspeechend = function() {
 recognition.onerror = function(event) {
 	message = 'Error occurred in recognition: ' + event.error;
 }
-
-
-
-
-
-
