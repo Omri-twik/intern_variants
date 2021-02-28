3877a743d4ed47b93eabe0ea1e155129cfb4984e
Tali Rozenman <73098343+rozenlady@users.noreply.github.com>
2021-02-28 17:19:35 +0200

    Update cartBtn.js

--- Tali R/cartBtn.js
+++ Tali R/cartBtn.js
@@ -1,20 +1,16 @@
 let selector = 'Your selector'; //single_add_to_cart_button button alt
-
 let copyBtn = document.querySelector(selector); 
-
-let stickyBtn = document.createElement('div')
-
-let stickyBtnStyle = window.getComputedStyle(copyBtn);
-
-
-$("head").append(`<style>
+let stickyBtn = window.getComputedStyle(copyBtn);
+// let stickyBtn = document.querySelector(selector); 
+// Create Style
+$('head').append(`<style>
 <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
-stickyBtn{
+#stickyBtn{
   align-items: center;
   justify-content: center;
-  height: ${stickyBtnStyle['height']};
-  background-color: ${stickyBtnStyle['background-color']};
-  color:  ${stickyBtnStyle['color']}; 
+  height: ${stickyBtn['height']};
+  background-color: ${stickyBtn['background-color']};
+  color:  ${stickyBtn['color']}; 
   width: 60px;
   left: 0px; 
   top: 45%;
@@ -23,20 +19,35 @@ stickyBtn{
   margin-top: 50%;
   position:fixed;
 }
-
 #stickyBtn .toggle-btn span{
   cursor:pointer;
-
-#copBtn{
+}
+#copyBtn{
   display: none;
+  margin-top: 50%;
+  display: none;
+  position:fixed;
 }
-}
-
 </style>`);
 
-stickyBtn.style.cssText = stickyBtnStyle
+// Create new div for stickyButton
+$('body').append(
+  `<div id="stickyBtn"><i class="fas fa-shopping-basket"></i></div>
+  <div id="copyBtn" >
+      <div CLASS="panel_title" class="copyBtn"></div>
+      <div CLASS="panel_item">
+          <template:UserControl id="ucCategories" src="UserControls/ProductCategories.ascx"
+          />
+      </div>
+  </div>`);
 
-document.body.appendChild(stickyBtn);
+// clone Pay Button inside new div
+  const init = function(){
+    let divBtn = document.getElementById('copyBtn');
+    cloneBtn = document.querySelector('.copyBtn');
+        divBtn.appendChild(cloneBtn.cloneNode(true) );
+  }
+  document.addEventListener('DOMContentLoaded', init)
 
 // Make StickyBtn Visible
 let stuck = false;
@@ -53,26 +64,10 @@ window.onscroll = function(e) {
   }
 }
 
-// Toggle Sticky Btn append script
-// let stickyScript = document.createElement("script");
-// stickyScript.type = "text/javascript";
-// stickyScript.src = "https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js";
 
-$("body").append(
-  `<div id="stickyBtn"><i class="fas fa-shopping-basket"></i></div>
-  <div id="copyBtn">
-      <div CLASS="panel_title" class="copyBtn"></div>
-      <div CLASS="panel_item">
-          <template:UserControl id="ucCategories" src="UserControls/ProductCategories.ascx"
-          />
-      </div>
-  </div>
-  `
-);
-
-$("head").append(stickyScript);
 // toggle sticky Btn function
-$('.stickyBtn').click(function () {
+$('head').append(stickyScript);
+$('#stickyBtn').click(function () {
   if($('stickyBtn').is(':visible')){
   $('stickyBtn').fadeOut(function () {
       $('#copyBtn').toggle('slide', {
@@ -86,3 +81,12 @@ $('.stickyBtn').click(function () {
       }, 1000, function(){ $('stickyBtn').fadeIn();});
   }
 });
+
+
+
+
+// Toggle Sticky Btn append script
+// let stickyScript = document.createElement("script");
+// stickyScript.type = "text/javascript";
+// stickyScript.src = "https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js";
+
