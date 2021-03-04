f8aab7fb475096c3ef35c5addd027051bcd081fa
Tali Rozenman <73098343+rozenlady@users.noreply.github.com>
2021-03-04 17:10:48 +0200

    Update cartBtn.js

--- Tali R/cartBtn.js
+++ Tali R/cartBtn.js
@@ -1,11 +1,12 @@
 let selector = '#product-4457 > div.summary.entry-summary.has-no-bid-product.has_no_sale_price > form > button'
 // Get the element
 let copyBtn = document.querySelector(selector);
-// Create a copy of it
+// Clone selector 
 let cloneBtn = copyBtn.cloneNode(true);
+cloneBtn.addEventListener('click', () => {copy.click()});
 
 // // append jquery link to head
-
+//----------------------
 if (window.jQuery) {
     $ = window.jQuery;
 } else {
@@ -20,28 +21,43 @@ if (window.jQuery) {
     };
     document.getElementsByTagName("head")[0].appendChild(script);
 }
-
+//------------------------------------
+// STYLE
 
 let stickyBtn = window.getComputedStyle(copyBtn);
-  
 $('head').append(`<style>
 <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
 #cloneBtnId{
-  left: 0px;
-  top: 50%;
   z-index: 999;
   transition:all 300ms linear;
   margin-top: 50%;
-  position:fixed;
-
-  width:${stickyBtn['width']};
-  display: block;
+  position: fixed !important;
+  height:67px;
+  margin: auto;
+  left: 0px;
+  top: 50%;
+  position: fixed;
+  z-index: 9999;
+  display: none;
 }
-
 #stickyBtn{
     margin: auto;
+    display: block;
+    background-color: ${stickyBtn['background-color']};
+    color:  ${stickyBtn['color']};
+    padding: 11px 20px 11px 20px;
+    margin: auto;
+    font-weight:${stickyBtn['font-weight']};
+    font-size: 30px;
+    display: block;
+    left: 0px;
+    top: 50%;
+    position: fixed;
+    border-radius: 3px;
+    background:${stickyBtn['background']};
+    font-family:${stickyBtn['font-family']};
+    z-index: 9999;
 }
-
 #stickyBlock{
     height:67px;
     background-color: ${stickyBtn['background-color']};
@@ -55,94 +71,68 @@ $('head').append(`<style>
     top: 50%;
     position: fixed;
     border-radius: 3px;
-
     background:${stickyBtn['background']};
     font-family:${stickyBtn['font-family']};
     z-index: 9999;
 }
-
 #stickyBlock:hover{ 
     cursor: pointer;
 }
-
-#copyBtn{
-  display: none;
-  margin-top: 50%;
-  display: none;
-  position:fixed;
-}
-
 </style>`);
-
-
-// // Inject it into the DOM
+//-----------------------------------------
 // // Create new div for stickyButton
 $('body').append(
-    `<div  id="stickyBlock">
-     <i class="fas fa-shopping-basket"  id="stickyBtn"></i>
-    </div>
-    <div id='cloneBtnId'> </div>`);
-
-    
-let purchaseBtn = document.getElementById("cloneBtnId");
-purchaseBtn.innerHTML += cloneBtn ;
-
-
+    `<div  id="stickyBlock" class="block">
+        <i class="fas fa-shopping-basket"  id="stickyBtn"></i>
+        <div id='cloneBtnId'> </div>
+    </div>`
+);
+//---------------------------
+//append clone div to div
+let div = document.getElementById("cloneBtnId");
+div.appendChild(cloneBtn);
+//---------------------------
+//append clone to div
+$("#cloneBtnId").append(cloneBtn);
+//-----------------------------------------------
 // Visibility Function
 $.fn.inView = function(){
     if(!this.length) 
         return false;
     var rect = this.get(0).getBoundingClientRect();
-
     return (
         rect.top >= 0 &&
         rect.bottom <= (window.innerHeight || document.documentElement.clientHeight)
     );
-
 };
-
-// Additional examples for other use cases
-// Is true false whether an array of elements are all in view
-$.fn.allInView = function(){
-    var all = [];
-    this.forEach(function(){
-        all.push( $(this).inView() );
-    });
-    return all.indexOf(false) === -1;
-};
-
-// Only the class elements in view
-$('.some-class').filter(function(){
-    return $(this).inView();
-});
-
-// Only the class elements not in view
-$('.some-class').filter(function(){
-    return !$(this).inView();
-});
-
-
+//--------------------------------------------------------------
+//window scroll Function
 $(window).on('scroll',function(){
 
     if($(copyBtn).inView() ) {
-        // Do cool stuff
         stickyBlock.style.display = 'none';
         stuck = true;
     } else {
         stickyBlock.style.display = 'block'; 
     }
 });
+//--------------------------------------------------------------
+//mouse hover 
 
-
-$(document).ready(function () {
-    $('#cloneBtnId').hide();
-    var el = '#stickyBlock';
-    $('#stickyBlock').hover(function () {
-        $(el).stop(true, true).fadeOut("slow", function () {
-            el = el == '#stickyBlock' ? '#cloneBtnId' : '#stickyBlock';
-            $(el).stop(true, true).fadeIn();
-        });
+$(document).ready(function() {
+    $('#stickyBlock').on('hover', function() {
+        let icon = document.getElementById("stickyBtn");
+        let iconStatus = icon.style.display;
+        let btn = document.getElementById("cloneBtnId");
+        let btnStatus = btn.style.display;
+    
+        if (iconStatus === "block") {
+            icon.style.display = "none";
+            btn.style.display = "block";
+        } else {
+            icon.style.display = "block";
+            btn.style.display = "none";
+        }
     });
 });
 
-let test = document.querySelector("#stickyBlock")
