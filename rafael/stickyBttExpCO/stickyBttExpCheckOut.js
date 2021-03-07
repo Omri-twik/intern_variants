f6372eff25ad5596f99a22c4ac8a52f10f89981c
rgrac <gracia.rafael@hotmail.com>
2021-03-07 17:21:21 +0200

    updated with fully working, still working in the recognizing wich icon to use automatically

--- rafael/stickyBttExpCO/stickyBttExpCheckOut.js
+++ rafael/stickyBttExpCO/stickyBttExpCheckOut.js
@@ -1,89 +1,140 @@
-let selector = "enter your selector here";
-let rootDiv = document.getElementById('root');
-let coButtons = document.querySelector(selector);
-let contentDiv = document.getElementById('content');
-let stickyDiv = document.createElement('div');
-let iconCart = document.createElement('span');
-let rootElement = document.documentElement;
-let cssStyle = document.createElement('style');
-cssStyle.innerHTML = `
-.CheckOutButtons {
-    display: flex;
+if (window.jQuery) {
+    $ = window.jQuery;
+    mainJS()
+} else {
+    var script = document.createElement("SCRIPT");
+    script.src =
+        "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js";
+    script.type = "text/javascript";
+    // this is doc.ready
+    //-------------------
+    script.onload = function() {
+        var $ = window.jQuery;
+        mainJS()
+    };
+    document.getElementsByTagName("head")[0].appendChild(script);
 }
 
-.sticky {
-    display: -webkit-box;
-    -webkit-box-align: center;
-    -webkit-box-pack: center;
-    width: min-content;
-    padding: 0 1vw;
-    height: 6vh;
-    margin-top: 25%;
-    position: fixed;
-    align-items: center;
-    justify-content: center;
-    display: flex;
-    top: 0;
-    left: 0;
-    bottom: 0;
-    opacity: 1;
-    background-color: orangered;
-    z-index: 1;
-    border-radius: 0 25% 25% 0;
-}
-
-.iconCart {
-    font-size: 30px;
-    color: white;
-}
-
-.expand {
-    /* transition: min-content 0.5s ease; */
-    padding: 0 1vw;
-    border-radius: 0 5% 5% 0;
-    /* width: 20%; */
-    /* padding: 0 5px; */
-    height: 6vh;
-    width: min-content;
-    transform: scaleX(1);
-    transform-origin: left;
-}
-
-.sticky.hide {
-    overflow: hidden;
-    transition:transform 1s ease-in;
-    opacity: 0;
-    transform: scaleX(0);
-}
-`;
-let fontAwesome = `<script async defer src="https://kit.fontawesome.com/08ab31cae7.js" crossorigin="anonymous"></script>`;
-document.head.insertAdjacentHTML("beforeend", fontAwesome);
-document.head.appendChild(cssStyle);
-iconCart.classList.add('iconCart');
-iconCart.innerHTML = `<i class="fas fa-shopping-cart"></i>`;
-stickyDiv.appendChild(iconCart);
-stickyDiv.classList.add('sticky', 'hide');
-rootDiv.appendChild(stickyDiv);
-function handleScroll() {
-    let scrollTotal = rootElement.scrollHeight - rootElement.clientHeight;
-    if ((rootElement.scrollTop / scrollTotal) > 0.05) {
-        stickyDiv.classList.remove('hide');
-    } else {
-        stickyDiv.classList.add('hide');
-        rootDiv.insertBefore(coButtons);
-        stickyDiv.classList.remove('expand');
+function mainJS() {
+    let fontAwesome = `<script async defer src="https://kit.fontawesome.com/08ab31cae7.js" crossorigin="anonymous"></script>`;
+    document.head.insertAdjacentHTML("beforeend", fontAwesome);
+    let cssStyle = document.createElement('style')
+    document.head.appendChild(cssStyle);
+    cssStyle.innerHTML = `
+    .customSticky {
+        background-color: orangered;
+        display: -webkit-box;
+        -webkit-box-align: center;
+        -webkit-box-pack: center;
+        width: 5vw;
+        padding: 0 1vw;
+        height: fit-content;
+        margin-top: 50%;
+        position: fixed;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        top: 0;
+        left: 0;
+        bottom: 0;
+        opacity: 1;
+        border-radius: 0 10px 10px 0;
+        transition: all 0.5s ease-in;
+        transform-origin: left;
+        
     }
+    
+    .iconCart {
+        font-size: 30px;
+        color: white;
+    }
+    
+    .expand {
+        transition: all 0.5s ease-in;
+        transform-origin: left;
+        border-radius: 0 5% 5% 0;
+        display: flex;
+        justify-content: center;
+        align-content: center;
+    }
+    
+    .targetElement {
+        margin: auto !important;
+        top: 0 !important;
+        bottom: 0 !important;
+        left: 0 !important;
+        right: 0 !important;
+    }
+    
+    .hide {
+        opacity: 0;
+    }
+    `
+    let customStickyDiv = document.createElement('div');
+    customStickyDiv.id = 'customStickyDiv';
+    document.body.appendChild(customStickyDiv)
+    let iconCart = document.createElement('span');
+    iconCart.classList.add('iconCart');
+    iconCart.innerHTML = `<i class="fas fa-shopping-cart"></i>`;
+    customStickyDiv.appendChild(iconCart);
+    customStickyDiv.classList.add('customSticky', 'hide');
+    
+    //replace by your selector
+    let selector = '.single_add_to_cart_button'
+    //
+    let targetElement = document.querySelector(selector)
+    let rect = targetElement.getBoundingClientRect();
+    targetElement.classList.add('targetElement');
+    let targetBackground = window.getComputedStyle(targetElement);
+    customStickyDiv.style.backgroundColor = targetBackground.getPropertyValue('background-color')
+    
+
+    console.log(selector.classList)
+    //cloning the selector to avoid the removechild to remove the main selector via DOM
+    let clonedSelector = $(targetElement).clone(true)
+    console.log(clonedSelector);
+    //returns if the object is the client view
+    function isItVisible(targetElement) {
+        const rect = targetElement.getBoundingClientRect();
+        return (
+            rect.top >= 0 &&
+            rect.left >= 0 &&
+            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+            rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+            )
+        };
+        
+        document.addEventListener("scroll", function () {
+            if (isItVisible(targetElement)) {
+                customStickyDiv.classList.add('hide');
+                console.log(`I'm in`);
+            } else {
+                customStickyDiv.classList.remove('hide');
+                // clonedSelector.style.display = 'none'
+                console.log(`I'm out`)
+            }
+        });
+        
+        $(clonedSelector).click(function(){
+            $(targetElement).trigger("click")
+        })
+
+        customStickyDiv.addEventListener('mouseenter', event => {
+            customStickyDiv.classList.add('expand');
+            setTimeout(function () {
+                document.querySelector('.expand').style.width = `${rect.width+20}px`;
+                document.querySelector('.expand').style.height = `${rect.height+20}px`;
+                $(customStickyDiv).append(clonedSelector)
+            }, 400);
+            iconCart.style.visibility = 'hidden';
+        })
+        
+        customStickyDiv.addEventListener('mouseleave', event => {
+            customStickyDiv.classList.remove('expand');
+            customStickyDiv.style.removeProperty('width');
+            customStickyDiv.style.removeProperty('height');
+            clonedSelector.remove()
+            iconCart.style.visibility = 'visible';
+        })
+    /////////////////////WAS WORKING TILL HERE ^^^^^^^^^^^^^
 }
-document.addEventListener("scroll", handleScroll);
-
-stickyDiv.addEventListener('mouseenter', event => {
-    stickyDiv.classList.add('expand');
-    stickyDiv.appendChild(coButtons);
-    stickyDiv.removeChild(iconCart);
-})
-
-stickyDiv.addEventListener('mouseleave', event => {
-    stickyDiv.classList.remove('expand');
-    stickyDiv.removeChild(coButtons);
-    stickyDiv.appendChild(iconCart);  
-})
