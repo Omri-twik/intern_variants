98eff1123cd87379a6c06a7558e68df461b4b069
DenisSkulovic <dskulovich@gmail.com>
2021-03-07 17:14:03 +0200

    made some progress on fixing the broken variant

--- denis_the_terrible/fixing_broken_variant/fixing_broken_variant.js
+++ denis_the_terrible/fixing_broken_variant/fixing_broken_variant.js
@@ -4,14 +4,146 @@
 
 // Intellisense
 
-
-
-var maxSuggestions = 5;
+var maxCookieSuggestions = 3;
+var maxGoogleSuggestions = 5;
+var suggestionsHistoryCookieName = "searchHistory";
+var cookieExpirationInDays = 1;
 
 var url = "https://suggestqueries.google.com/complete/search?client=firefox&q=";
 
-var searchBox = ".input-container > .input-confirm";
-var searchInput = ".input-container > .input-confirm > .input > input";
+var searchBoxSelector =
+  "body > div.row.fixed-top > div > div > nav > div:nth-child(3)";
+var searchInputSelector = "#searchCard";
+
+document.head.insertAdjacentHTML(
+  "beforeend",
+  `
+  <style>
+
+    #suggestions-list-intellisense {
+      display: none; 
+      position: fixed;
+      background-color: green;
+    }
+
+    .intellisenseSuggestionsRow {
+      background-color: red;
+    }
+
+  </style>
+`
+);
+
+document.body.insertAdjacentHTML(
+  "beforeend",
+  `
+    <ul id="suggestions-list-intellisense">
+    </ul>
+`
+);
+
+var searchBox = document.querySelector(searchBoxSelector);
+var inputElement = document.querySelector(searchInputSelector);
+var suggestionsList = document.querySelector("#suggestions-list-intellisense");
+
+function getCookies() {
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var cookies = decodedCookie.split(";");
+  let cookiesObject = {};
+  for (cookie of cookies) {
+    var [name, value] = cookie.split("=");
+    cookiesObject[name] = value;
+  }
+  return cookiesObject;
+}
+
+function appendRowToSuggestionsList(value) {
+  let suggestionsUl = document.querySelector("#suggestions-list-intellisense");
+  let newRow = document.createElement("li");
+  newRow.classList.add("intellisenseSuggestionsRow");
+  newRow.innerHTML = value;
+  suggestionsUl.appendChild(newRow);
+}
+
+function clearSuggestionsList() {
+  let suggestionsUl = document.querySelector("#suggestions-list-intellisense");
+  suggestionsUl.innerHTML = "";
+}
+
+function unhideSuggestionsList() {
+  if (!document.querySelector("#intellisenseSuggestionsList_style")) {
+    document.head.insertAdjacentHTML(
+      "beforeend",
+      `
+    <style id="intellisenseSuggestionsList_style">
+      #suggestions-list-intellisense {
+        display: flex;
+        flex-direction: column;
+        flex-wrap: nowrap;
+      }
+    </style>
+    `
+    );
+  }
+}
+
+function hideSuggestionsList() {
+  if (document.querySelector("#intellisenseSuggestionsList_style")) {
+    document.querySelector("#intellisenseSuggestionsList_style").remove();
+  }
+}
+
+function resizeSuggestionsList() {
+  if (
+    suggestionsUl.style.top !==
+    window.scrollY + inputElement.getBoundingClientRect().bottom + "px"
+  ) {
+    suggestionsUl.style.top =
+      window.scrollY + inputElement.getBoundingClientRect().bottom + "px";
+  }
+
+  if (
+    suggestionsUl.style.left !==
+    window.scrollX + inputElement.getBoundingClientRect().left + "px"
+  ) {
+    suggestionsUl.style.left =
+      window.scrollX + inputElement.getBoundingClientRect().left + "px";
+  }
+
+  if (
+    suggestionsUl.style.width !== window.getComputedStyle(inputElement).width
+  ) {
+    suggestionsUl.style.width = window.getComputedStyle(inputElement).width;
+  }
+}
+
+function setSuggestionRowsEventListeners() {
+  let suggestionRows = document.querySelectorAll(".intellisenseSuggestionsRow");
+  for (let row of suggestionRows) {
+    row.addEventListener("click", (e) => {
+      e.preventDefault();
+      let selectionValue = e.target.textContent;
+
+      // insert selection into search field and clear suggestions
+      inputElement.value = selectionValue;
+      clearSuggestionsList();
+      hideSuggestionsList();
+
+      // add selection to cookie
+      let suggestionsHistory_string = cookies[suggestionsHistoryCookieName];
+      let suggestionsHistory_array = JSON.parse(suggestionsHistory_string);
+      suggestionsHistory_array.push(selectionValue);
+      let newSuggestionsHistory_string = JSON.stringify(
+        suggestionsHistory_array
+      );
+      setCookie(
+        suggestionsHistoryCookieName,
+        newSuggestionsHistory_string,
+        cookieExpirationInDays
+      );
+    });
+  }
+}
 
 // Function for getting JSONP.
 function jsonp(uri) {
@@ -38,6 +170,23 @@ function jsonp(uri) {
   });
 }
 
+function getSuggestions() {
+  var cookies = getCookies();
+
+  // Create list of suggestions based on google results and search history.
+  let suggestionsHistory_string = cookies[suggestionsHistoryCookieName];
+  let suggestionsHistory_array = JSON.parse(suggestionsHistory_string);
+
+  var suggestions = suggestionsHistory_array.filter((value) =>
+    value.includes(e.target.value)
+  );
+  suggestions = suggestions.concat(
+    res.filter((value) => !suggestions.includes(value))
+  );
+
+  return suggestions;
+}
+
 // Sets cookie and cookie's expiration date.
 // Denis's comments: apparently expireIn is in days
 function setCookie(name, value, expireIn) {
@@ -53,61 +202,36 @@ function setCookie(name, value, expireIn) {
     window.location.hostname;
 }
 
-// Gets cookie's value.
-// Denis's comments: undefined: searchHistory, intellisnseUl
-// decodeURIComponent replaces each UTF-8 escape sequence with the characters it represents
-function getCookie(name) {
-var cookieName = name + "=";
-var decodedCookie = decodeURIComponent(document.cookie);
-var cookies = decodedCookie.split(';');
-var inputElement = document.querySelector(searchInput);
-if (!document.querySelector("#suggestions-list-intellisense")) {
-  var intellisnseUl = document.createElement("ul");
-  intellisnseUl.id = "suggestions-list-intellisense";
-  intellisnseUl.style.display="none";
-  intellisnseUl.style.position = "fixed";
-  document.body.appendChild(intellisnseUl);
-
-}
-for (var i = 0; i < maxSuggestions; i++) {
+for (var i = 0; i < maxGoogleSuggestions; i++) {
   let suggestion = cookies[i];
-if (intellisnseUl.style.top !== window.scrollY + inputElement.getBoundingClientRect().bottom + "px") {
-intellisnseUl.style.top = window.scrollY + inputElement.getBoundingClientRect().bottom + "px";
-}
-
-if (intellisnseUl.style.left !== window.scrollX + inputElement.getBoundingClientRect().left + "px") {
-intellisnseUl.style.left = window.scrollX + inputElement.getBoundingClientRect().left + "px";
-}
-
-if (intellisnseUl.style.width !== window.getComputedStyle(inputElement).width) {
-intellisnseUl.style.width = window.getComputedStyle(inputElement).width;
+  let suggestionLi = document.createElement("li");
+  suggestionLi.innerHTML = suggestion;
+  suggestionsUl.appendChild(suggestionLi);
 }
 
 // Get google autocomplete results.
-jsonp(url + e.target.value)
-.then(res => res[1])
-.then(res => {
-// Display list.
-intellisnseUl.style.display = "block";
-// Remove all previous children from list.
-while (intellisnseUl.firstChild) {
-intellisnseUl.removeChild(intellisnseUl.firstChild);
-}
+searchInputSelector.addEventListener("keydown", (e) => {
+  jsonp(url + e.target.value)
+    .then((res) => res[1])
+    .then((res) => {
+      // SUCCESS
 
-// Create list of suggestions based on google results and search history.
-var suggestions = searchHistory.filter(value => value.includes(e.target.value));
-suggestions = suggestions.concat(res.filter(value => !suggestions.includes(value)));
+      // Clear and unhide list.
+      clearSuggestionsList();
+      unhideSuggestionsList();
 
-// Add all suggestions to list.
-for (var i = 0; i {
-inputElement.value = e.target.innerHTML;
-intellisnseUl.style.display = "none";
-currentFocus = -1;
-})
+      let suggestions = getSuggestions();
 
-// Add suggestions to list
-intellisnseUl.appendChild(listItem);
-}
-})
-// If error, don't display list.
-.catch(() => { intellisnseUl.style.display = "none" })
+      for (let suggestion of suggestions) {
+        appendRowToSuggestionsList(suggestion);
+      }
+
+      resizeSuggestionsList();
+      setSuggestionRowsEventListeners();
+    })
+    .catch(() => {
+      // ERROR
+      clearSuggestionsList();
+      hideSuggestionsList();
+    });
+});
