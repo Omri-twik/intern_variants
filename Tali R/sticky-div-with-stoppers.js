ef2cb25dd35bf03bba0731a9f996dacba4f9092e
DenisSkulovic <dskulovich@gmail.com>
2021-03-10 09:42:24 +0200

    added some comments for Tali

--- Tali R/sticky-div-with-stoppers.js
+++ Tali R/sticky-div-with-stoppers.js
@@ -1,4 +1,4 @@
-let selector =  'body > div.content > div > div.sidebar > div.sidebar__content';
+let selector = "body > div.content > div > div.sidebar > div.sidebar__content";
 
 // Get the element------------------------------
 let selectedElem = document.querySelector(selector);
@@ -6,22 +6,32 @@ let selectedElem = document.querySelector(selector);
 // get element's style---------------------------
 let selectedElemStyle = window.getComputedStyle(selectedElem);
 
+// ####################################################################
+// message from Denis:
+// the code block below should be used differently. As of now it only appends jQuery.
+// It has to both append jQuery and fire jQuery functions only when jQuery is loaded.
+// The way you have it now will give errors as you will be executing jQuery before it loaded.
+// You need to put all of your code into a function, for example mainJS(),
+// and put it in two spots in the code block below.
+// This function has to be activated after lines "$ = window.jQuery;" and after "var $ = window.jQuery;"
+// ####################################################################
+
 // append jquery link to head/----------------
 if (window.jQuery) {
-    $ = window.jQuery;
+  $ = window.jQuery;
 } else {
-    var script = document.createElement("SCRIPT");
-    script.src =
-        "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js";
-    script.type = "text/javascript";
-    script.onload = function() {
-        var $ = window.jQuery;
-    };
-    document.getElementsByTagName("head")[0].appendChild(script);
+  var script = document.createElement("SCRIPT");
+  script.src =
+    "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js";
+  script.type = "text/javascript";
+  script.onload = function () {
+    var $ = window.jQuery;
+  };
+  document.getElementsByTagName("head")[0].appendChild(script);
 }
 
 // STYLE---------------------------------------------------
-$('head').append(`<style>
+$("head").append(`<style>
 .float-attribute{
     z-index: 9999;
     position: fixed;
@@ -33,12 +43,15 @@ $('head').append(`<style>
 $(selector).attr("class", "float-attribute");
 
 //stop view
-$(document).scroll(function() {
-    if($(selector).offset().top + $(selector).height() >= $('footer').offset().top - 10)
-        $(selector).css({'position': 'fixed', 'bottom' : 0,});
-        
-    if($(document).scrollTop() + $(selector).height() < $('footer').offset().top)
-        $(selector).css({'position': 'fixed', 'bottom' : 'auto'}); // restore when you scroll up
+$(document).scroll(function () {
+  if (
+    $(selector).offset().top + $(selector).height() >=
+    $("footer").offset().top - 10
+  )
+    $(selector).css({ position: "fixed", bottom: 0 });
+
+  if ($(document).scrollTop() + $(selector).height() < $("footer").offset().top)
+    $(selector).css({ position: "fixed", bottom: "auto" }); // restore when you scroll up
 });
 
 // //Closest Element
@@ -47,7 +60,7 @@ $(document).scroll(function() {
 
 // //  Visibility Function -------------------------
 // $.fn.inView = function(){
-//     if(!this.length) 
+//     if(!this.length)
 //         return false;
 //     var rect = this.get(0).getBoundingClientRect();
 //     return (
@@ -55,16 +68,12 @@ $(document).scroll(function() {
 //         rect.bottom <= (window.innerHeight || document.documentElement.clientHeight)
 //     );
 // };
-    
+
 // //window scroll display parameters ------------------------------
 // $(window).on('scroll',function(){
 //     if(scrollDisplay.inView() ) {
 //         scrollObj.style.display = 'none';
 //     } else {
-//         scrollObj.style.display = 'block'; 
+//         scrollObj.style.display = 'block';
 //     }
 // });
-
-
-
-
