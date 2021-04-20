87c78e5b8646f9aaec800a1db2034f68363287a4
DenisSkulovic <dskulovich@gmail.com>
2021-04-20 16:34:58 +0300

    clal fix

--- denis_the_terrible/clalSearchSuggestions/clalSearchSuggestions.js
+++ denis_the_terrible/clalSearchSuggestions/clalSearchSuggestions.js
@@ -231,77 +231,10 @@ function main_js() {
     });
   }
 
-  // fixing broken search icon functionality on mobile
-  function openSearchMobile() {
-    try {
-      document.body.classList.add("bodyFixed");
-      document.querySelector(".Header").classList.add("HeaderHeightOpenPopup");
-      document
-        .querySelector(".SearchBarSection.hide-desktop")
-        .classList.remove("animatedElement");
-      document
-        .querySelector(".SearchBarSection.hide-desktop input#SearchBarInput")
-        .classList.add("inputFocus");
-      document
-        .querySelector(
-          `[ng-class="{'BgPopUp' : showSearchPopup || searchData.displayContactUsSection || searchData.selectedInsurance >=0}"]`
-        )
-        .classList.add("BgPopUp");
-      document
-        .querySelector(
-          `[ng-class="{'BgPopUp' : showSearchPopup || searchData.displayContactUsSection || searchData.selectedInsurance >=0}"]`
-        )
-        .addEventListener("click", closeSearchMobile);
-      document.querySelectorAll(".SearchBarSection").forEach((el) => {
-        el.style.setProperty("min-height", "100px");
-        el.querySelector(".SearchBarLeft").style.setProperty(
-          "min-height",
-          "100px"
-        );
-        el.querySelector(".SearchBarLeft")
-          .querySelectorAll(".PopularLinksContianer")
-          .forEach((el2) => {
-            el2.style.setProperty("display", "none");
-          });
-      });
-    } catch {}
-  }
-
   function clearInputFields() {
     document.querySelectorAll("#SearchBarInput").forEach((inp) => {
       inp.value = "";
     });
-  }
-
-  function closeSearchMobile() {
-    try {
-      document.body.classList.remove("bodyFixed");
-      document
-        .querySelector(".Header")
-        .classList.remove("HeaderHeightOpenPopup");
-      document
-        .querySelector(".SearchBarSection.hide-desktop")
-        .classList.add("animatedElement");
-      document
-        .querySelector(
-          `[ng-class="{'BgPopUp' : showSearchPopup || searchData.displayContactUsSection || searchData.selectedInsurance >=0}"]`
-        )
-        .classList.remove("BgPopUp");
-      document.querySelector(
-        `[ng-class="{'BgPopUp' : showSearchPopup || searchData.displayContactUsSection || searchData.selectedInsurance >=0}"]`
-      ),
-        removeEventListener("click", closeSearchMobile);
-      document.querySelectorAll(".SearchBarSection").forEach((el) => {
-        el.style.removeProperty("min-height");
-        el.querySelector(".SearchBarLeft").style.removeProperty("min-height");
-        el.querySelector(".SearchBarLeft")
-          .querySelectorAll(".PopularLinksContianer")
-          .forEach((el2) => {
-            el2.style.removeProperty("display");
-          });
-      });
-      clearInputFields();
-    } catch {}
   }
 
   function addOrRemoveSearchButtonFunctionalityMobile() {
@@ -314,19 +247,6 @@ function main_js() {
         .querySelector("#HaederSearchBar")
         .removeEventListener("click", openSearchMobile);
     }
-  }
-
-  // .SearchBarSection.hide-desktop
-  if (
-    window.getComputedStyle(
-      document.querySelector(".SearchBarSection.hide-desktop")
-    )["position"] === "static"
-  ) {
-    addOrRemoveSearchButtonFunctionalityMobile();
-    window.addEventListener("resize", () => {
-      closeSearchMobile();
-      addOrRemoveSearchButtonFunctionalityMobile();
-    });
   }
 
   function filterSuggestionsUL(textValue) {
@@ -507,15 +427,6 @@ function main_js() {
   );
 
   let ul = document.querySelector("#suggestions-list-clal");
-
-  // fixing broken label on mobile search functionality
-  for (let searchDiv of document.querySelectorAll(".SearchBarInputContianer")) {
-    let input = searchDiv.querySelector("input");
-    let label = searchDiv.querySelector("label");
-    label.addEventListener("click", () => {
-      input.focus();
-    });
-  }
 
   // inserting rows and storing them in an array
   suggestions.forEach((val) => {
